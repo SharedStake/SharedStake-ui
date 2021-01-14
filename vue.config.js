42e80c510ef977f66307ada0878d262282adeb99
sharedStake-main <defiavatar@gmail.com>
2021-01-14 22:58:53 +0300

    aws didn't deployed fix1

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,5 @@
 module.exports = {
     devServer: {
         port: 8080
-    },
-    chainWebpack: config => {
-        config.module
-            .rule('html')
-            .test(/\.html$/)
-            .use('html-loader')
-            .loader('html-loader')
     }
 }
