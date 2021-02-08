ab7cf9d694b7687c92fae563d0dd4f038a2a409a
sharedStake-main <defiavatar@gmail.com>
2021-02-08 12:51:43 +0300

    price up

--- src/components/Landing/Landing.vue
+++ src/components/Landing/Landing.vue
@@ -119,7 +119,7 @@
           </span>
           <span class="stat">
             <div class="miniStats">SGT price</div>
-            <div>20.45$</div>
+            <div>23.45$</div>
             <a
               class="loginButton SGT"
               href="https://info.uniswap.org/pair/0x3d07f6e1627da96b8836190de64c1aed70e3fc55"
