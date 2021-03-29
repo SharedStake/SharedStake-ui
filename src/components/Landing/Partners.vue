6851f2c84c453488d304f0233735e1d7350bbbc3
exitonoff <devausmm@gmail.com>
2021-03-30 02:57:14 +0300

    Create partners component and partner objects

---
+++ src/components/Landing/Partners.vue
@@ -0,0 +1,106 @@
+<template>
+  <div>
+    <div class="header">Partners</div>
+    <div class="partner-container">
+      <PartnerItem
+        class="partner-item" 
+        v-for="partner in partners" :key="partner.key"
+        :name="partner.name" 
+        :text="partner.text" 
+        :imageUrl="partner.imageUrl"
+        :link="partner.link" />
+    </div>
+  </div>
+</template>
+
+<script>
+import PartnerItem from "./PartnerItem";
+
+export default {
+  components: { PartnerItem },
+  data() {
+    return {
+      partners: [
+        {
+          name: "UniSwap",
+          imageUrl: "uniswap.png",
+          text: "SGT liquidity pools are on UniSwap.",
+          link: "https://info.uniswap.org/pair/0x3d07f6e1627DA96B8836190De64c1aED70e3FC55/",
+          key: 1
+        },
+        {
+          name: "Ruler",
+          imageUrl: "ruler.png",
+          text: "Use Ruler to get loans using vETH2 as collateral!",
+          link: "https://rulerprotocol.com/",
+          key: 2
+        },
+        {
+          name: "Chainlink",
+          imageUrl: "chainlink.png",
+          text: "Price oracle partner for SharedStake.",
+          link: "https://chain.link/",
+          key: 3
+        },
+      ],
+    }
+  }
+}
+</script>
+
+<style scoped>
+.header {
+  color: rgb(255, 255, 255);
+  align-items: center;
+  font-size: 50px;
+  line-height: 1.16em;
+  margin: 30px;
+  font-weight: 300;
+  -webkit-animation: EnterFromBelow 0.4s ease-out backwards;
+  animation: EnterFromBelow 0.4s ease-out backwards;
+  text-align: center;
+  margin-bottom: 10vh;
+}
+
+.partner-container {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+
+  margin: 0px auto 30px;
+  padding: 0px;
+  -webkit-animation: EnterFromBelow 0.4s ease-out backwards 0.2s;
+  animation: EnterFromBelow 0.4s ease-out backwards 0.2s;
+}
+
+.partner-item {
+  width: 30%;
+}
+
+@media only screen and (max-width: 1024px) {
+  .partner-item {
+    width: 45%;
+    margin-bottom: 7vh;
+  }
+}
+
+@media only screen and (max-width: 768px) {
+  .partner-item {
+    width: 80%;
+    margin-bottom: 3vh;
+  }
+}
+
+@keyframes EnterFromBelow {
+  from {
+    transform: translate3d(0, 2rem, 0);
+    opacity: 0;
+  }
+
+  to {
+    transform: translate3d(0, 0, 0);
+    opacity: 1;
+  }
+}
+
+</style>
