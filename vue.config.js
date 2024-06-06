960af2fecc9d16673858f57b129c5efb2f303fd8
Cihmera Defi <chimera_defi@protonmail.com>
2024-06-05 21:49:20 -0400

    fix build

--- vue.config.js
+++ vue.config.js
@@ -8,9 +8,9 @@ module.exports = {
         config.module
           .rule('js')
           .test(/\.js$/)
-          .exclude
-            .add(/node_modules(?!\/@web3-onboard\/core)/)  // Exclude all node_modules except @web3-onboard/core
-            .end()
+        //   .exclude
+        //     .add(/node_modules(?!\/@web3-onboard\/core)/)  // Exclude all node_modules except @web3-onboard/core
+        //     .end()
           .use('babel-loader')
           .loader('babel-loader')
           .options({
